67bfef1f4bbf62d726022f7e7b38c08523ba6ba4
JohannesStroebele <stroebele.j@gmail.com>
2020-07-21 11:57:00 +0200

    Implemented working login, but fix needed for real form and footer on all pages

--- public/index.js
+++ public/index.js
@@ -2,3 +2,16 @@
 $(function(){
     $("#nav-placeholder").load("nav.html");
 });
+
+function myFunction() {
+    let email = "admin@gmail.com";
+    let password = "password";
+
+    if (document.getElementById("password").value === password && document.getElementById("email").value === email){
+        console.log('Success');
+        location.href='/dashboard';
+    } else {
+        console.log('Failure');
+        location.href='/';
+    }
+}
