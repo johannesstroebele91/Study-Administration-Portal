8e0dce7fc81ebe11489b8f85b2840c864dd4acaf
JohannesStroebele <stroebele.j@gmail.com>
2020-07-21 22:11:17 +0200

    Fix minor dashboard issue, worked on exams page (table, buttons), and added table to study progress

---
+++ public/javascripts/exams.js
@@ -0,0 +1,41 @@
+let courses = [
+    {
+        registered: true,
+        id: 53423,
+        name: "App Design",
+        lastName: "20.07.2020",
+        degree: "ID",
+    },
+    {
+        registered: true,
+        id: 53423,
+        name: "App Design",
+        lastName: "20.07.2020",
+        degree: "ID",
+    },
+    {
+        registered: true,
+        id: 53423,
+        name: "App Design",
+        lastName: "20.07.2020",
+        degree: "ID",
+    },
+    {
+        registered: true,
+        id: 53423,
+        name: "App Design",
+        lastName: "20.07.2020",
+        degree: "ID",
+    },
+    {
+        registered: true,
+        id: 53423,
+        name: "App Design",
+        lastName: "20.07.2020",
+        degree: "ID",
+    },
+];
+
+// TODO implement using: https://stackoverflow.com/questions/171027/add-table-row-in-jquery
+// and https://stackoverflow.com/questions/18333427/how-to-insert-a-row-in-an-html-table-body-in-javascript
+$('#table-mock-data > tbody:last-child').append('<tr><th>Deregister</th></tr><tr><th>Deregister</th></tr>');
