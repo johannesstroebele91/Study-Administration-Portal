de4856f274e490a27606a85bf1668ca97316a0b8
JohannesStroebele <stroebele.j@gmail.com>
2020-07-21 12:13:41 +0200

    Finished login, added logos, but footer on all pages needs to be fixed

--- public/index.js
+++ public/index.js
@@ -1,17 +1,21 @@
 // Navigation on multiple pages
-$(function(){
+$(function () {
     $("#nav-placeholder").load("nav.html");
 });
 
-function myFunction() {
+// Login
+function submitForm() {
+
     let email = "admin@gmail.com";
     let password = "password";
 
-    if (document.getElementById("password").value === password && document.getElementById("email").value === email){
-        console.log('Success');
-        location.href='/dashboard';
+    if (document.getElementById("password").value === password && document.getElementById("email").value === email) {
+        location.href = '/dashboard';
+    } else if (document.getElementById("password").value === password) {
+        console.log('Password is not "password"');
+    } else if (document.getElementById("email").value === email) {
+        console.log('Email is not "admin@gmail.com"');
     } else {
-        console.log('Failure');
-        location.href='/';
+        console.log('Other issue');
     }
 }
